76181370125dccca9d880ac521f24dca00a0e1f7
anagiu <126169431+anagiu@users.noreply.github.com>
2023-06-22 09:57:51 -0300

    busca

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -8,8 +8,20 @@ use Illuminate\Validation\Rule;
 
 class ProdutosController extends Controller
 {
-    public function index() {
-        $prods = Produto::all();
+    public function index(Request $request){
+        if ($request->isMethod('POST')){
+            $busca = $request->busca;
+
+            $ord = $request->ord == 'asc' ? 'asc' : 'desc';
+
+            $prods = Produto::where('name', 'LIKE', "%{$busca}%")->orderBy('name', $ord)->get();
+
+            }else{
+                $prods = Produto::all();
+
+        }
+
+
 
         # Busca tudo com apagados
         # $prods = Produto::withTrashed()->get();
